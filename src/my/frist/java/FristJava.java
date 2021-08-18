bd0799a6888fa7ff495056d1187b332ee7f6427d
liubotao <13228023861@163.com>
2021-08-18 22:42:15 +0800

    我的第二次

--- src/my/frist/java/FristJava.java
+++ src/my/frist/java/FristJava.java
@@ -3,5 +3,6 @@ package my.frist.java;
 public class FristJava {
     public static void main(String[] args) {
         System.out.println("hellow World 1");
+        System.out.println("hellow World 2");
     }
 }
