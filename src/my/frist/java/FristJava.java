da010559d3fec8344f2f24bdce5b0d7ba6c241dd
liubotao <13228023861@163.com>
2021-08-19 21:54:51 +0800

    hot-fix commit1

--- src/my/frist/java/FristJava.java
+++ src/my/frist/java/FristJava.java
@@ -4,5 +4,6 @@ public class FristJava {
     public static void main(String[] args) {
         System.out.println("hellow World 1");
         System.out.println("hellow World 2");
+        System.out.println("hellow World 6");
     }
 }
