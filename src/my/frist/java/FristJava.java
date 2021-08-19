31945a8dc468719b145b589ee36bc2a6e7d308e1
liubotao <13228023861@163.com>
2021-08-19 21:58:51 +0800

    hot-fix commit2

--- src/my/frist/java/FristJava.java
+++ src/my/frist/java/FristJava.java
@@ -5,5 +5,6 @@ public class FristJava {
         System.out.println("hellow World 1");
         System.out.println("hellow World 2");
         System.out.println("hellow World 6");
+        System.out.println("hot-fix test");
     }
 }
